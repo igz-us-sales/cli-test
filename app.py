9681c6da502ad581aac6d741644760f6862e8a1b
Nick Schenone <nschenone16@gmail.com>
2022-06-02 22:10:49 +0000

    Updated app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import click
+import mlrun
 
 @click.command()
 @click.option('--count', default=1, help='Number of greetings.')
@@ -7,6 +8,10 @@ def hello(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
         click.echo(f"Hello {name}!")
+        
+    context = mlrun.get_or_create_ctx("train")
+    context.logger.info("mlrun logging")
+    context.log_result("accuracy", 0.93)
 
 if __name__ == '__main__':
     hello()
